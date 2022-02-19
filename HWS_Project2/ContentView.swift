7a2a5a1880ee467364039862edc1a22ca2ccc6fb
Arjun <arjundoel@gmail.com>
2022-02-19 11:12:45 +0000

    Initial Commit

---
+++ HWS_Project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS_Project2
+//
+//  Created by Arjun Doel on 19/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS_Project2/HWS_Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Project2App.swift
+//  HWS_Project2
+//
+//  Created by Arjun Doel on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
